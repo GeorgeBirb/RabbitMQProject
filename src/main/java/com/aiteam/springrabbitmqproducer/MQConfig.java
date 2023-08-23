b33881e494fd19bda92cb37f4c6bf056b3c3c7b7
George Birbakos <niraeus@gmail.com>
2023-08-23 18:20:34 +0300

    Initial Commit

---
+++ src/main/java/com/aiteam/springrabbitmqproducer/MQConfig.java
@@ -0,0 +1,31 @@
+package com.aiteam.springrabbitmqproducer;
+
+import com.rabbitmq.client.AMQP;
+import org.springframework.amqp.core.Binding;
+import org.springframework.amqp.core.BindingBuilder;
+import org.springframework.amqp.core.TopicExchange;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.amqp.core.Queue;
+
+@Configuration
+public class MQConfig {
+
+    public static final String QUEUE = "message_queue";
+    public static final String EXCHANGE = "message_exchange";
+    public static final String ROUTING_KEY = "message_routingKey";
+
+    @Bean
+    public Queue queue(){
+      return new Queue(QUEUE);
+    }
+
+    @Bean
+    public TopicExchange exchange(){
+        return new TopicExchange(EXCHANGE);
+    }
+
+    public Binding binding(Queue queue, TopicExchange exchange){
+        return BindingBuilder.bind(queue).to(exchange).with(ROUTING_KEY);
+    }
+}
